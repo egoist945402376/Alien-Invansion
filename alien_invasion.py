f47b7abfa3bfec93d4e24d869b428803da2226db
egoist945402376 <egoist945402376@gmail.com>
2023-06-19 10:40:36 +0800

    6.19 Update

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,15 +2,31 @@ import sys
 
 import pygame
 
+from settings import Settings
+from Ship import Ship
+
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+
+    ai_settings = Settings()
+
+    # Set_mode 方法返回整个游戏窗口
+    screen = pygame.display.set_mode((ai_settings.screen_weight, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建飞船
+    ship = Ship(screen)
+
+    # background color
+    bg_color = ai_settings.bg_color
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(bg_color)
+        ship.blitme()
 
         pygame.display.flip()
 
